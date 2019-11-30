ea6cd8a57203b839cf5abe37ee31dcde65980267
Seoksoon Jang <boraseoksoon@gmail.com>
2019-11-30 21:10:40 +0900

    [ADD] README

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -30,21 +30,45 @@ import XCTest
 @testable import Numero
 
 class ConverterTests: XCTestCase {
-  var converter: Converter!
-
-  override func setUp() {
-    super.setUp()
-    converter = Converter()
-  }
-
-  override func tearDown() {
-    converter = nil
-    super.tearDown()
-  }
-
-  func testConversionForOne() {
-    let result = converter.convert(1)
-    XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
-  }
+    var converter: Converter!
+    
+    override func setUp() {
+        super.setUp()
+        converter = Converter()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        converter = nil
+    }
+    
+    func testConversionForOne() {
+        let result = converter.convert(1)
+        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+    }
 }
 
+//import XCTest
+//@testable import Numero
+//
+//class ConverterTests: XCTestCase {
+//    var converter: Converter!
+//
+//    override func setUp() {
+//        super.setUp()
+//        converter = Converter()
+//    }
+//
+//    override func tearDown() {
+//        converter = nil
+//        super.tearDown()
+//    }
+//
+//    func testConversionForOne() {
+//        let result = converter.convert(1)
+//        XCTAssertEqual(result, "I", "Conversion for 1 is incorrect")
+//    }
+//
+//
+//}
+//
